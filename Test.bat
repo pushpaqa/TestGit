a70c99d7c0254c71ad506ab5a5a5481159d3b873
Rajitha <rajitha220@gmail.com>
2018-01-31 14:55:52 +0530

    Batch file added

---
+++ Test.bat
@@ -0,0 +1,2 @@
+cd C:\Users\rajitha.g\.jenkins\workspace\Vidhisinghania_Ar
+java -cp C:\Users\rajitha.g\.jenkins\workspace\Vidhisinghania_Ar\lib*;C:\Users\rajitha.g\.jenkins\workspace\Vidhisinghania_Ar\bin org.testng.TestNG testng.xml
